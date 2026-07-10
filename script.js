97467391951ec16ba05d7c2216cda5d3bf9c654e
FreshOil-web <tsimakwaneame@gmail.com>
2026-07-10 21:41:39 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -169,3 +169,31 @@ if(copyright){
 copyright.innerHTML=`© ${new Date().getFullYear()} Fresh Oil Ministries | All Rights Reserved`;
 
 }
+
+const loginBtn = document.getElementById("openLogin");
+const popup = document.getElementById("loginPopup");
+const closeBtn = document.getElementById("closePopup");
+
+loginBtn.addEventListener("click", function(e){
+
+    e.preventDefault();
+
+    popup.style.display = "flex";
+
+});
+
+closeBtn.addEventListener("click", function(){
+
+    popup.style.display = "none";
+
+});
+
+window.addEventListener("click", function(e){
+
+    if(e.target === popup){
+
+        popup.style.display = "none";
+
+    }
+
+});
